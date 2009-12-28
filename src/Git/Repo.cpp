1c5468c9e6fa790b5ca36259b4695085f7af6f74
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-12-28 04:43:40 +0100

    Roughly implemented Repo::containsRepository().

--- src/Git/Repo.cpp
+++ src/Git/Repo.cpp
@@ -2,6 +2,10 @@
 
 
 
+#include <QDir>
+
+
+
 using namespace Git;
 
 
@@ -11,8 +15,42 @@ Repo::Repo(QObject *parent)
 {
 }
 
-bool Repo::containsRepository(const QString &path)
+bool Repo::containsRepository(const QString &path, const RepoOptions options)
 {
+	QString repoPath = QDir(path).absolutePath();
+
+	QString bareRepoPath;
+	QString dotGitPath;
+
+	if (!(options & Bare)) {
+		dotGitPath = repoPath + "/.git";
+		if (QDir(dotGitPath).exists()) {
+			bareRepoPath = dotGitPath;
+		}
+	}
+
+	if (bareRepoPath.isNull()) {
+		bareRepoPath = repoPath;
+	}
+
+	QDir bareRepoDir = QDir(bareRepoPath);
+	if(bareRepoDir.exists()) { // "*.git" directory exists
+		QStringList dirs, files; // it should contain these dirs and files
+		dirs << "branches" << "objects" << "refs";
+		files << "config" << "description" << "index";
+
+		QStringList entries = bareRepoDir.entryList();
+
+		bool isRepo = true; // will become false if only one item is missing
+
+		/** @todo actually distinguish between dirs and files */
+		foreach(const QString &item, dirs + files) {
+			isRepo = isRepo && entries.contains(item);
+		}
+
+		return isRepo;
+	}
+
 	return false;
 }
 
--- src/Git/Repo.h
+++ src/Git/Repo.h
@@ -9,10 +9,12 @@ namespace Git {
 
 
 
-enum RepoOptions {
-	Bare
+enum RepoOption {
+	None = 0,
+	Bare = 1<<1
 };
-Q_ENUMS(RepoOptions)
+Q_DECLARE_FLAGS(RepoOptions, RepoOption)
+Q_DECLARE_OPERATORS_FOR_FLAGS(RepoOptions)
 
 
 
@@ -24,7 +26,7 @@ class Repo : public QObject
 		explicit Repo(QObject *parent = 0);
 
 	// static
-		static bool containsRepository(const QString &path);
+		static bool containsRepository(const QString &path, const RepoOptions options = None);
 
 	private:
 		QString workingDir;
