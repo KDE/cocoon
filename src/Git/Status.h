48d33a0ebc2baa52511eb8e53c1f6ace0edbc441
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-01-05 15:14:22 +0100

    Added Git::Status class.

---
+++ src/Git/Status.h
@@ -0,0 +1,45 @@
+/*
+	Cocoon - A GUI for Git.
+	Copyright (C) 2009  Riyad Preukschas <riyad@informatik.uni-bremen.de>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef STATUS_H
+#define STATUS_H
+
+#include <QObject>
+
+
+
+namespace Git {
+
+class Repo;
+
+
+
+class Status : public QObject
+{
+	Q_OBJECT
+
+	public:
+		explicit Status(const Repo *repo);
+
+	private:
+		const Repo *m_repo;
+};
+
+}
+
+#endif // STATUS_H
