7c3437469ca93bf6db822ee6bc6a2db4e8175961
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-01-27 19:34:04 +0100

    Added Git::ObjectStorage as abstract base class for all object storages.

---
+++ src/Git/ObjectStorage.cpp
@@ -0,0 +1,35 @@
+/*
+	Cocoon - A GUI for Git.
+	Copyright (C) 2009  Riyad Preukschas <riyad@informatik.uni-bremen.de>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ObjectStorage.h"
+
+#include "Repo.h"
+
+using namespace Git;
+
+
+
+ObjectStorage::ObjectStorage(Repo *repo)
+	: QObject(repo)
+	, m_repo(repo)
+{
+}
+
+
+
+#include "ObjectStorage.moc"
