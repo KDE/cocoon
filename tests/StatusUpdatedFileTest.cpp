a576b9a865d10cf827f1556d23c7e7a7f5d9339d
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-03-02 23:35:06 +0100

    Reworked StatusUpdatedFileTest.

--- tests/StatusUpdatedFileTest.cpp
+++ tests/StatusUpdatedFileTest.cpp
@@ -87,13 +87,13 @@ void StatusUpdatedFileTest::testUpdatedFile_lsFiles()
 	QVERIFY(status->lsFiles().size() == 1);
 
 	Git::StatusFile *file = status->lsFiles()[0];
-	QVERIFY(file->idIndex() == "86e041dad66a19b9518b83b78865015f62662f75");
+	QCOMPARE(file->idIndex(), QString("86e041dad66a19b9518b83b78865015f62662f75"));
 	QVERIFY(file->idRepo().isNull());
 	QVERIFY(!file->isStaged());
-	QVERIFY(file->modeIndex() == "100644");
+	QCOMPARE(file->modeIndex(), QString("100644"));
 	QVERIFY(file->modeRepo().isNull());
-	QVERIFY(file->path() == "updated.txt");
-	QVERIFY(file->status() == Git::StatusFile::None);
+	QCOMPARE(file->path(), QString("updated.txt"));
+	QCOMPARE(file->status(), Git::StatusFile::None);
 }
 
 void StatusUpdatedFileTest::testUpdatedFile_diffFiles()
@@ -106,13 +106,13 @@ void StatusUpdatedFileTest::testUpdatedFile_diffIndex()
 	QVERIFY(status->diffIndex("HEAD").size() == 1);
 
 	Git::StatusFile *file = status->diffIndex("HEAD")[0];
-	QVERIFY(file->idIndex() == "86e041dad66a19b9518b83b78865015f62662f75");
-	QVERIFY(file->idRepo() == "a907ec3f431eeb6b1c75799a7e4ba73ca6dc627a");
+	QCOMPARE(file->idIndex(), QString("86e041dad66a19b9518b83b78865015f62662f75"));
+	QCOMPARE(file->idRepo(), QString("a907ec3f431eeb6b1c75799a7e4ba73ca6dc627a"));
 	QVERIFY(!file->isStaged());
-	QVERIFY(file->modeIndex() == "100644");
-	QVERIFY(file->modeRepo() == "100644");
-	QVERIFY(file->path() == "updated.txt");
-	QVERIFY(file->status() == Git::StatusFile::Modified);
+	QCOMPARE(file->modeIndex(), QString("100644"));
+	QCOMPARE(file->modeRepo(), QString("100644"));
+	QCOMPARE(file->path(), QString("updated.txt"));
+	QCOMPARE(file->status(), Git::StatusFile::Modified);
 }
 
 void StatusUpdatedFileTest::testUpdatedFile_diffUntrackedFiles()
@@ -129,18 +129,18 @@ void StatusUpdatedFileTest::testUpdatedFile_diffIgnoredFiles()
 
 void StatusUpdatedFileTest::testUpdatedFileHasStatus()
 {
-	QVERIFY(status->files().size() == 1);
+	QCOMPARE(status->files().size(), 1);
 	QVERIFY(!status->forFile("updated.txt").isEmpty());
 }
 
 void StatusUpdatedFileTest::testUpdatedFileIsModified()
 {
 	QList<Git::StatusFile*> fileStatus = status->forFile("updated.txt");
-	QVERIFY(fileStatus.size() == 1);
+	QCOMPARE(fileStatus.size(), 1);
 
 	Git::StatusFile *file = fileStatus[0];
 	QVERIFY(file->isModified());
-	QVERIFY(file->status() == Git::StatusFile::Modified);
+	QCOMPARE(file->status(), Git::StatusFile::Modified);
 }
 
 void StatusUpdatedFileTest::testUpdatedFileIsStaged()
@@ -158,28 +158,28 @@ void StatusUpdatedFileTest::testUpdatedFileDefaultBlobIsIndexBlob()
 {
 	Git::StatusFile *file = status->forFile("updated.txt")[0];
 
-	QVERIFY(file->blob() == file->blob("index"));
+	QCOMPARE(file->blob(), file->blob("index"));
 }
 
 void StatusUpdatedFileTest::testUpdatedFileFileBlobIsCorrect()
 {
 	Git::StatusFile *file = status->forFile("updated.txt")[0];
 
-	QVERIFY(file->blob("file") == "foo\nbar\nbaz\n");
+	QCOMPARE(file->blob("file"), QByteArray("foo\nbar\nbaz\n", 12));
 }
 
 void StatusUpdatedFileTest::testUpdatedFileIndexBlobIsCorrect()
 {
 	Git::StatusFile *file = status->forFile("updated.txt")[0];
 
-	QVERIFY(file->blob("index") == "foo\nbar\nbaz\n");
+	QCOMPARE(file->blob("index"), QByteArray("foo\nbar\nbaz\n", 12));
 }
 
 void StatusUpdatedFileTest::testUpdatedFileRepoBlobIsCorrect()
 {
 	Git::StatusFile *file = status->forFile("updated.txt")[0];
 
-	QVERIFY(file->blob("repo") == "foo\nbar");
+	QCOMPARE(file->blob("repo"), QByteArray("foo\nbar", 7));
 }
 
 void StatusUpdatedFileTest::testUpdatedFileDiffIsCorrect()
@@ -199,7 +199,7 @@ void StatusUpdatedFileTest::testUpdatedFileDiffIsCorrect()
 		assert_equal("--- a/updated.txt\n+++ b/updated.txt\n@@ -1,2 +1,3 @@\n foo\n-bar\n\\ No newline at end of file\n+bar\n+baz\n", d.diff)
 */
 	QVERIFY(!diff.isNull());
-	QVERIFY(diff == "--- a/updated.txt\n+++ b/updated.txt\n@@ -1,2 +1,3 @@\n foo\n-bar\n\\ No newline at end of file\n+bar\n+baz\n");
+	QCOMPARE(diff, QString("--- a/updated.txt\n+++ b/updated.txt\n@@ -1,2 +1,3 @@\n foo\n-bar\n\\ No newline at end of file\n+bar\n+baz\n"));
 }
 
 #include "StatusUpdatedFileTest.moc"
