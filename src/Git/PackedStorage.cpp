afb1aaafa2a87ad4b1e527263a72e23105922869
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-05-31 01:26:40 +0200

    Fixed values for PackObjectType enum.

--- src/Git/PackedStorage.cpp
+++ src/Git/PackedStorage.cpp
@@ -45,12 +45,12 @@ using namespace Git;
 
 typedef enum {
 	OBJ_NONE = 0,
-	OBJ_COMMIT, // = 1
-	OBJ_TREE, // = 2
-	OBJ_BLOB, // = 3
-	OBJ_TAG, // = 4
-	OBJ_OFS_DELTA, // = 6
-	OBJ_REF_DELTA, // = 7
+	OBJ_COMMIT = 1,
+	OBJ_TREE = 2,
+	OBJ_BLOB = 3,
+	OBJ_TAG = 4,
+	OBJ_OFS_DELTA = 6,
+	OBJ_REF_DELTA = 7,
 } PackObjectType;
 
 
