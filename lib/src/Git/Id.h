7990794e629dd7257ff739b03155673d6d061733
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-10-23 17:53:22 +0200

    Started documenting Id.

--- lib/src/Git/Id.h
+++ lib/src/Git/Id.h
@@ -16,6 +16,13 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * @file
+ * @author Riyad Preukschas <riyad@informatik.uni-bremen.de>
+ *
+ * A Git object's id.
+ */
+
 #ifndef ID_H
 #define ID_H
 
@@ -38,19 +45,65 @@ class Repo;
 
 
 
+/**
+ * @short A Git object's id.
+ *
+ * An object's id is a SHA1 hash of its content.
+ * It is also used to uniquely identify the object in the repository.
+ *
+ * You can create ids by supplying the full or a long enough (to be unique) part from the beginning of the id.
+ * The full id will be looked up in the reposiory.
+ *
+ * @code
+ *   Id("1234567890123456789012345678901234567890", repo)->toSha1String();
+ *   // will return "1234567890123456789012345678901234567890"
+ *   Id("1234567", repo)->toSha1String();
+ *   // will return "1234567890123456789012345678901234567890"
+ * @endcode
+ */
 class KDE_EXPORT Id : public QObject
 {
 	Q_OBJECT
 
 	public:
+		/**
+		 * @short Constructs an invalid Id.
+		 *
+		 * It does not exist and has no SHA1.
+		 * It is intended for indicating an error when retuning ids.
+		 *
+		 * @sa isValid()
+		 */
 		explicit Id();
+
+		/**
+		 * @short Copy constructor.
+		 */
 		Id(const Id &other);
+
+		/**
+		 * @short Constructs an object id.
+		 *
+		 * It will try to locate the storage the object is stored in.
+		 * If necessary it will find the full id of the object.
+		 *
+		 * @note Ids should be recreated after moving objects between storages (e.g. a `git gc` run).
+		 * @sa Id(const QString &sha1, ObjectStorage &storage)
+		 */
 		explicit Id(const QString &sha1, Repo &repo);
 		explicit Id(const QString &sha1, ObjectStorage &storage);
 		virtual ~Id();
 
 		bool exists() const;
 		bool isIn(const ObjectStorage &storage) const;
+
+		/**
+		 * @short Checks whether the id is valid.
+		 *
+		 * The id is invalid when it was constructed using the default constructor or when the object could not be found in the repo or was moved from the storage it was in once.
+		 *
+		 * @return true in case the id is valid, false otherwise
+		 */
 		bool isValid() const;
 		RawObject* object() const;
 		ObjectStorage& storage() const;
