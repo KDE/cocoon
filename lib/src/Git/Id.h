d83c1adbaa5d4bb8b7b4d744dc06c9144cc727ff
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-11-06 19:42:12 +0100

    Almost finished docs for Id.

--- lib/src/Git/Id.h
+++ lib/src/Git/Id.h
@@ -111,17 +111,115 @@ class KDE_EXPORT Id : public QObject
 		 * @brief Returns the object represented by the id.
 		 */
 		RawObject* object() const;
+
+		/**
+		 * @brief Returns the storage the object with this id is stored in.
+		 *
+		 * @return The object's storage.
+		 */
 		ObjectStorage& storage() const;
+
+		/**
+		 * @brief Returns the id's SHA1 in its 20 byte binary form.
+		 *
+		 * @return The 20 byte binary SHA1.
+		 */
 		const QByteArray toBinarySha1() const;
-		const QString&   toSha1String() const;
-		const QString    toShortSha1String() const;
-		const QString&   toString() const;
+
+		/**
+		 * @brief Returns the id's SHA1 in 40 character string form.
+		 *
+		 * The SHA1 is is represented as a 40 character hexadecimal string.
+		 *
+		 * @return The 40 character SHA1 string.
+		 *
+		 * @see toShortSha1String()
+		 */
+		const QString& toSha1String() const;
+
+		/**
+		 * @brief Returns a shortened representation of the ids SHA1.
+		 *
+		 * It will give you the first 7 characters of the SHA1 string.
+		 *
+		 * @return The first 7 characters of the SHA1 string.
+		 */
+		const QString toShortSha1String() const;
+
+		/**
+		 * @brief Returns the id's SHA1 in 40 character string form.
+		 *
+		 * The SHA1 is is represented as a 40 character hexadecimal string.
+		 *
+		 * @return The 40 character SHA1 string.
+		 *
+		 * @see toSha1String()
+		 */
+		const QString& toString() const;
 
 		int compare(const Id &other) const;
+
+		/**
+		 * @brief Compares two ids for numerical order.
+		 *
+		 * The comparison is based on the id's SHA1 value.
+		 *
+		 * @param other The other id.
+		 * @return true if the other id is numerically greater, false otherwise
+		 *
+		 * @note This is needed for Id to be used as key in QHash.
+		 */
 		bool operator<(const Id &other) const;
+
+		/**
+		 * @brief Assigns @a other to @a this.
+		 *
+		 * @param other The id to be assigned from.
+		 * @return @a this.
+		 */
 		Id&  operator=(const Id &id);
+
+		/**
+		 * @brief Compares two ids for equality.
+		 *
+		 * The comparison is based on the id's SHA1 value.
+		 *
+		 * @param other The id to be compared to.
+		 * @return true if both SHA1s are equal, false otherwise
+ *
+ * @note This is needed for Id to be used as key in QHash.
+		 */
 		bool operator==(const Id &other) const;
+
+		/**
+		 * @brief Compares an id with a string for equality.
+		 *
+		 * This is equivalent to:
+		 * @code
+		 *   id.toSha1String() == other;
+		 * @endcode
+		 *
+		 * The comparison is based on the id's full SHA1 value.
+		 * @code
+		 *   Id("1234567", repo) == "1234567890123456789012345678901234567890";
+		 *   // will return true
+		 *   Id("1234567", repo) == "1234567";
+		 *   // will always be false
+		 * @endcode
+		 *
+		 * @param other The string to be compared to.
+		 * @return true if the id's SHA1s and the string are equal, false otherwise
+		 */
 		bool operator==(const QString &other) const;
+
+		/**
+		 * @brief Compares two ids for inequality.
+		 *
+		 * The comparison is based on the id's SHA1 value.
+		 *
+		 * @param other The id to be compared to.
+		 * @return true if both SHA1s are not equal, false otherwise
+		 */
 		bool operator!=(const Id &other) const;
 
 	private:
@@ -134,7 +232,34 @@ class KDE_EXPORT Id : public QObject
 
 
 
+/**
+ * @brief Writes an Id to a stream.
+ *
+ * This is a convenience function so you can use Id directly in QDataStream.
+ * @code
+ *   qDebug() << "Can't load object with id" << id;
+ * @endcode
+ *
+ * @param stream The stream to write to.
+ * @param id The id to be written.
+ * @return A reference to the stream.
+ *
+ * @see #Id::toSha1String()
+ */
 KDE_EXPORT QDataStream& operator<<(QDataStream &stream, const Git::Id &id);
+
+
+/**
+ * @brief Computes a hash key from the Id.
+ *
+ * This function would be used by QHash if you wanted to build a hash table for instances of Id.
+ * As the id is already the hash value of the object it represents, the first bytes of the id are used as hash key here.
+ *
+ * @param id The id.
+ * @return The hash key.
+ *
+ * @note This is needed for Id to be used as key in QHash.
+ */
 KDE_EXPORT uint qHash(const Git::Id &id);
 
 
